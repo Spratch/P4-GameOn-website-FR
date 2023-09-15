1599083e7f5d909083027846fd5aac6363c017ef
Spratch <contact@josephclenet.fr>
2023-09-15 16:18:08 +0200

    Validation form

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,62 +1,77 @@
 // DOM Elements
 const form = document.querySelector("form");
 
+// Form values
+
+// let firstName = document.getElementById("first").value;
+// let lastName = document.getElementById("last").value;
+// let email = document.getElementById("email").value;
+// let birthdate = document.getElementById("birthdate").value;
+// let quantity = document.getElementById("quantity").value;
+// let location = document.getElementsByName("location").value;
+// let checkbox1 = document.getElementById("checkbox1").value;
+// let checkbox2 = document.getElementById("checkbox2").value;
+
 
 // Inputs validations
 // First name
-function firstValidation() {
-    let inputValue = document.getElementById("first").value;
-    if (inputValue !== null && inputValue.length >= 2) return true;
+function firstValidation(firstName) {
+    if (firstName !== null && firstName.length >= 2) return true;
 }
 // Last name
-function lastValidation() {
-    let inputValue = document.getElementById("last").value;
-    if (inputValue !== null && inputValue.length >= 2) return true;
+function lastValidation(lastName) {
+    if (lastName !== null && lastName.length >= 2) return true;
 }
 // Email
-function emailValidation() {
-    let inputValue = document.getElementById("email").value;
+function emailValidation(email) {
     let regex = RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
-    return regex.test(inputValue);
+    return regex.test(email);
 }
 // Quantity
-function quantityValidation() {
-    let inputValue = document.getElementById("quantity").value;
+function quantityValidation(quantity) {
     let regex = RegExp('^[0-9]+$');
-    return regex.test(inputValue);
+    return regex.test(quantity);
 }
 // Location
 function locationValidation() {
-    let radios = document.querySelectorAll('input[name="location"]')
+    let radios = document.querySelectorAll('input[name="location"]');
     for (let radio of radios) {
         if (radio.checked === true) return true
     }
     return false;
 }
 // Checkbox1
-function checkbox1Validation() {
-    let inputValue = document.getElementById("checkbox1").checked;
-    return inputValue;
+function checkbox1Validation(checkbox1) {
+    return checkbox1;
 }
 
+// Validate
+function validate() {
+    let formDatas = {
+        firstName: document.getElementById("first").value,
+        lastName: document.getElementById("last").value,
+        email: document.getElementById("email").value,
+        birthdate: document.getElementById("birthdate").value,
+        quantity: document.getElementById("quantity").value,
+        location: document.querySelector('input[name="location"]:checked').value,
+        checkbox1: document.getElementById("checkbox1").checked,
+        checkbox2: document.getElementById("checkbox2").checked
+    }
+    
 
-// submit
+    if (firstValidation(formDatas.firstName) 
+        && lastValidation(formDatas.lastName) 
+        && emailValidation(formDatas.email) 
+        && quantityValidation(formDatas.quantity) 
+        && locationValidation() 
+        && checkbox1Validation(formDatas.checkbox1)) {
+            console.log(formDatas);
+    }
+}
+
+// Submit
 form.addEventListener("submit", (event) => {
     // prevent default
     event.preventDefault();
-
-    let formValid = false;
-
-    if (firstValidation() 
-        && lastValidation() 
-        && emailValidation() 
-        && quantityValidation() 
-        && locationValidation() 
-        && checkbox1Validation()) {
-            formValid = true;
-    }
-
-    if (formValid) {
-        form.submit();
-    }
+    validate();
 });
